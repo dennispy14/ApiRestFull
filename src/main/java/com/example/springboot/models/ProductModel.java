18f49dadbc25d8a2aca88e25e4f313d99ce0bae1
Dennis de Oliveira Py <dennispy14@hotmail.com>
2023-10-12 19:30:21 -0300

    Criação de Api RestFull

---
+++ src/main/java/com/example/springboot/models/ProductModel.java
@@ -0,0 +1,27 @@
+package com.example.springboot.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.util.UUID;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Entity
+@Data
+@Table(name="TB_PRODUCTS")
+public class ProductModel extends RepresentationModel<ProductModel> implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID idProduct;
+    private String name;
+    private BigDecimal value;
+
+
+
+}
